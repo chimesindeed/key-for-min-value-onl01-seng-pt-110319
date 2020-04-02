74b07a9d3cfa24c70c980922be0921cb300f1c81
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-04-02 18:57:53 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  unless name_hash == nil
+    name_hash.sort_by {|k, v| v}.first
+  end
 end
